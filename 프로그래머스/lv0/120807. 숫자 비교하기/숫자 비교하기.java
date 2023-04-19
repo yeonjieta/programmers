86198c259ea0d5e1fb1b67702b3808e4d605b8fe
yeonji <108457705+yeonjieta@users.noreply.github.com>
2023-04-19 16:17:10 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int result=0;
+        if(num1 == num2){
+            result = 1;
+        }
+        else result = -1;
+        
+        return result;
+    }
+}
