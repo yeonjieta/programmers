d2110e2c91e2ec22d264890683d15843a60e4c50
yeonji <108457705+yeonjieta@users.noreply.github.com>
2023-05-12 11:10:26 +0900

    [unrated] Title: rny_string, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = rny_string.replace("m", "rn");
+        // replace() 메서드를 이용해 m > rn 
+        return answer;
+    }
+}
