b056625062ad6cdb5920e4cd97c27e3b3cb4d603
yeonji <108457705+yeonjieta@users.noreply.github.com>
2023-05-16 14:38:45 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.37 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        Arrays.sort(num_list);
+        // 오름차순으로 정렬하고
+        answer = Arrays.copyOfRange(num_list, 5, num_list.length);
+        return answer;
+    }
+}
