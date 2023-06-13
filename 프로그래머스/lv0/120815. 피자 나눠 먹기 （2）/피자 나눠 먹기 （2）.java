3345f46aca7f9d38b803276b6a0d7b7da17e3d7f
yeonji <108457705+yeonjieta@users.noreply.github.com>
2023-06-13 20:05:26 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -4,7 +4,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-    for(int i = 1; i < 100; i ++){
+    for(int i = 1; i <= 100; i ++){
         if((n * i) % 6 == 0){
             // 피자를 6명에게 공정하게 나눠주기 위해서 n명 * i(반복문을 통해 달라지는 값)를 하다
             // 그 값이 6의 배수가 되면 
