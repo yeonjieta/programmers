f1d4548365e2230b5323c2ea9f0b492764058ddc
yeonji <108457705+yeonjieta@users.noreply.github.com>
2023-09-24 10:16:32 +0900

    [unrated] Title: 특수문자 출력하기, Time: 112.64 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[] args) {
+        // 따옴표나 홑따옴표 출력을 위해선 \ 를 앞에 추가해줘야한다.
+        // 테스트 결과 역슬래쉬 출력을 위해 \\' 이렇게 홑따옴표를 위한 역슬래쉬와
+        // 겹쳐도 되고 \\\' 이런식으로 원래대로 써도 가능
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+        
+    }
+}
