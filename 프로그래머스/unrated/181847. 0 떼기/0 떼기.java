b8102d5c7a4a66b82e527da421ebb9e0b7f8af3b
yeonji <108457705+yeonjieta@users.noreply.github.com>
2023-10-03 17:22:36 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,25 +1,8 @@
-import java.util.*;
-
-// 0이 아닌 수가 나올 때 까지 0을 제거
-// 앞에서 0 을 제거하다, 
 class Solution {
     public String solution(String n_str) {
         String answer = "";
-        //
-        char[] ch = n_str.toCharArray();
-        for(int i = 0; i < ch.length; i++){
-            // 처음에 0이 나오면 0을 빈문자로 대체한다.
-            if(ch[i] != '0'){
-                answer = n_str;
-                break;
-            }
-            else{
-                n_str = n_str.replaceFirst("0",""); 
-                answer = n_str;
-            }
-             
-                
-        }
+        int str = Integer.parseInt(n_str);
+        answer = String.valueOf(str);
         return answer;
 }
 }
